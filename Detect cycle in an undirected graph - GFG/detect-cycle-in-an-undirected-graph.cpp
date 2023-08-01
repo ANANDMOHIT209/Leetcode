841cbf2b376b550f3429888cc41f5f3e250fc0bc
MOHIT ANAND <76389527+ANANDMOHIT209@users.noreply.github.com>
2023-08-01 20:23:07 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -6,33 +6,47 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool f(int i,vector<int>adj[],vector<int>&vis){
+    bool f(int i,int parent,vector<int>adj[],vector<int>&vis){
+        // vis[i]=1;
+        // queue<pair<int,int>>q;
+        // q.push({i,-1});
+        // while(!q.empty()){
+        //     int node=q.front().first;
+        //     int parent=q.front().second;
+        //     q.pop();
+        //     for(auto it:adj[node]){
+        //         if(!vis[it]){
+        //             vis[it]=1;
+        //             q.push({it,node});
+        //         }
+        //         else if(parent!=it){
+        //             return true;
+        //         }
+        //     }
+            
+        // }
+        // return false;
+        
+        // DFS
+        
         vis[i]=1;
-        queue<pair<int,int>>q;
-        q.push({i,-1});
-        while(!q.empty()){
-            int node=q.front().first;
-            int parent=q.front().second;
-            q.pop();
-            for(auto it:adj[node]){
+        for(auto it:adj[i]){
                 if(!vis[it]){
-                    vis[it]=1;
-                    q.push({it,node});
+                   if(f(it,i,adj,vis)) return true;
                 }
                 else if(parent!=it){
                     return true;
                 }
             }
-            
-        }
         return false;
     }
+    
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
         vector<int>vis(V,0);
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                if(f(i,adj,vis)) return true;
+                if(f(i,-1,adj,vis)) return true;
             }
         }
         return false;
