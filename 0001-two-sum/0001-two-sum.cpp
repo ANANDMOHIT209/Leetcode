9a6c7d98ce5e9e6f66052b69d364e1daeb02a3d7
MOHIT ANAND <76389527+ANANDMOHIT209@users.noreply.github.com>
2023-08-16 14:45:12 +0530

    Time: 8 ms (88.90%), Space: 10.8 MB (44.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,24 +3,33 @@ public:
     vector<int> twoSum(vector<int>& nums, int t) {
         
         vector<pair<int,int>>p;
-        for(int i=0;i<nums.size();i++){
-            p.push_back({nums[i],i});
-        }
-        sort(p.begin(),p.end());
-        int f=0;
-        int l=p.size()-1;
+//         for(int i=0;i<nums.size();i++){
+//             p.push_back({nums[i],i});
+//         }
+//         sort(p.begin(),p.end());
+//         int f=0;
+//         int l=p.size()-1;
         
-        while(f<l){
-            int sum=0;
-            sum=p[f].first+p[l].first;
-            // cout<<sum<<" ";
-            if(sum==t){
-                return {p[f].second,p[l].second};
+//         while(f<l){
+//             int sum=0;
+//             sum=p[f].first+p[l].first;
+//             // cout<<sum<<" ";
+//             if(sum==t){
+//                 return {p[f].second,p[l].second};
+//             }
+//             if(sum>t)
+//                 l--;
+//             if(sum<t)
+//                 f++;
+//         }
+//         return {};
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            int x=t-nums[i];
+            if(mp.find(x)!=mp.end()){
+                return {mp[x],i};
             }
-            if(sum>t)
-                l--;
-            if(sum<t)
-                f++;
+            mp[nums[i]]=i;
         }
         return {};
     }
