099874e3c0699309ec4786bcf83adfc1d96f9731
MOHIT ANAND <76389527+ANANDMOHIT209@users.noreply.github.com>
2023-09-15 20:30:14 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -8,27 +8,26 @@ class Solution
     public:
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
-    bool static comp(pair<pair<int,int>,int> a, pair<pair<int,int>,int> b){
-        if(a.first.second<b.first.second) return true;
-        else if(a.first.second>b.first.second) return false;
-        // else if(a.second<b.second) return true;
+    bool static comp(pair<int,int> a, pair<int,int> b){
+        if(a.second<b.second) return true;
+        else if(a.second>b.second) return false;
         else return false;
     }
     int maxMeetings(int start[], int end[], int n)
     {
         // Your code here
-        vector<pair<pair<int,int>,int>>v;
+        vector<pair<int,int>>v;
         for(int i=0;i<n;i++){
-            v.push_back({{start[i],end[i]},i});
+            v.push_back({start[i],end[i]});
         }
         sort(v.begin(),v.end(),comp);
         int res=1;
-        int mini=v[0].first.second;
+        int mini=v[0].second;
         for(int i=1;i<n;i++){
             // cout<<v[i].first.first<<" "<<v[i].first.second<<" "<<v[i].second<<endl;
-            if(v[i].first.first>mini){
+            if(v[i].first>mini){
                 res++;
-                mini=v[i].first.second;
+                mini=v[i].second;
             }
         }
         return res;
